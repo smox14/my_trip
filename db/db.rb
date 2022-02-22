61e0797ee99cc782d11bfd78fcd7625188de0079
smox14 <kantarat.htkkw@gmail.com>
2022-02-22 12:33:54 +1100

    prep for deploy

--- db/db.rb
+++ db/db.rb
@@ -1,8 +1,8 @@
 require 'pg'
 
 def run_sql(sql, sql_params = [])
-  db = PG.connect(dbname: 'my_trip_db')
+  PG.connect(ENV['DATABASE_URL'] || {dbname: 'my_trip_db'})
   results = db.exec_params(sql, sql_params)
   db.close
   results
-end
+end 
